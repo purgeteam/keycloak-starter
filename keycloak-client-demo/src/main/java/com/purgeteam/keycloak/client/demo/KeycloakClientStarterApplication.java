7a4c8bd3942499163dc368f979e45db6a771a232
Purgeyao <zhaoy@deepblueai.com>
2019-10-03 23:14:40 +0800

    add swagger.properties

--- keycloak-client-demo/src/main/java/com/purgeteam/keycloak/client/demo/KeycloakClientStarterApplication.java
+++ keycloak-client-demo/src/main/java/com/purgeteam/keycloak/client/demo/KeycloakClientStarterApplication.java
@@ -1,9 +1,11 @@
 package com.purgeteam.keycloak.client.demo;
 
 import io.purge.starter.dispose.annotation.EnableGlobalDispose;
+import io.purge.swagger.annotation.EnableSwaggerPlugins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSwaggerPlugins
 @EnableGlobalDispose
 @SpringBootApplication
 public class KeycloakClientStarterApplication {
